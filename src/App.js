cd2858de8bd23a61beb7f510b0daecb94dc8182c
Heytemisan <temyicy@gmail.com>
2021-12-30 09:59:27 +0100

    added Punklist.css

--- src/App.js
+++ src/App.js
@@ -22,11 +22,6 @@ function App() {
   return (
     <div className="app">
         <Header/>
-        <CollectionCard
-          id={0}
-          name={'temisan'}
-          traits={[{'value':7}]}
-          image="https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu"/>
         <Punklist punkListData={punkListData}/>
     </div>
   );
