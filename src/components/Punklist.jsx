4a0f83a6a09b31172bee7a7778222856634d0e8b
Heytemisan <temyicy@gmail.com>
2021-12-30 07:47:10 +0100

    added  Punklist.jsx

---
+++ src/components/Punklist.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CollectionCard from './CollectionCtard'
+
+const Punklist = () => {
+    return (
+        <div className="punkList">
+            {
+                punkListData.map(punk => (
+                    <div>
+                        
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Punklist
