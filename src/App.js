3b2d7551f0b8f6827e7ab3d85902348c08e092c1
Heytemisan <temyicy@gmail.com>
2021-12-30 06:06:21 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
+
 function App() {
   return (
     <div className="app">
         <Header/>
-        <CollectionCard/>
+        <CollectionCard
+          id={0}
+          name={temisan}
+          traits={[{'value':7}]}
+          image="https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu"/>
     </div>
   );
 }
