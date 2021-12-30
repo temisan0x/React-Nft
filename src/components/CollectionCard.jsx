60b745512ff66c2d0ccd349a8fbe6c2a4b0ae104
Heytemisan <temyicy@gmail.com>
2021-12-30 06:00:10 +0100

    updated CollectionCard.jsx

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const CollectionCard = () => {
+const CollectionCard = ({id, name, traits, image}) => {
     return (
-        <div>
-            
+        <div className="collectionCard">
+            <img src={image} alt="collectionImage" />
+            <div className="details"></div>
+            <div className="name">
+                {name}
+                <div className="id">
+                    .#{od}
+                </div>
+            </div>
+            <div className="priceContainer">
+                <img src={eth} alt="ethImage" className="ethImage" />
+                <div className="price">
+                    {traits[0]?.value}
+                </div>
+            </div>
         </div>
     )
 }
