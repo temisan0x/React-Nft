1f8c9ac04fd6a8983112ae0e9a97b83e33bc0b64
Heytemisan <temyicy@gmail.com>
2021-12-30 06:01:00 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
         <Header/>
+        <CollectionCard/>
     </div>
   );
 }
