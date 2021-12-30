f24ee94c8d298f244d4f8cade2ed752b950c620b
Heytemisan <temyicy@gmail.com>
2021-12-30 13:10:05 +0100

    modified Punklist.jsx

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       ('https://testnets-api.opensea.io/assets?asset_contract_address=0xb4ad07f6E4c0d0d7dE2e7F1d085Fcd76D6f2ee0a&order_direction=asc'
       ) 
       console.log(openseaData.data.assets);
-      setpunkListData(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
     }
     return getMyNfts();
   }, [])
@@ -24,7 +24,7 @@ function App() {
     <div className="app">
         <Header/>
         <Main/>
-        <Punklist punkListData={punkListData} setSelectedPunk={setSelectedPunk}/>
+        <Punklist punkListData={punkListData} setSelectedPunk={setPunkListData}/>
     </div>
   );
 }
